69e7e34f074939db129b6fb6cce16eb63e1b83c0
tamakiii <tamakiii@users.noreply.github.com>
2023-04-12 13:13:57 +0900

    init docker.mk

---
+++ docker.mk
@@ -0,0 +1,17 @@
+.PHONY: help build bash clean
+
+IMAGE := tamakiii-sandbox/hello-langchain
+LABEL := latest
+WORK_DIR := /usr/local/lib/$(IMAGE)
+
+help:
+	@cat $(firstword $(MAKEFILE_LISt))
+
+build: Dockerfile
+	docker build -t $(IMAGE):$(LABEL) .
+
+bash:
+	docker run -it --rm -v $(PWD):$(WORK_DIR) -w $(WORK_DIR) $(IMAGE):$(LABEL) $@
+
+clean:
+	docker image rm $(IMAGE):$(LABEL)
