db08511be155fd068a6f192ce58b8657200409cb
tamakiii <tamakiii@users.noreply.github.com>
2023-04-13 21:02:50 +0900

    print llm_result

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from langchain.llms import OpenAI
 
 llm = OpenAI(model_name="text-ada-001", n=2, best_of=2)
 print(llm("Tell me a joke"))
+
+llm_result = llm.generate(["Tell me a joke", "Tell me a poem"]*15)
+print(len(llm_result.generations))
+print(llm_result.generations[0])
+print(llm_result.generations[-1])
+print(llm_result.llm_output)
+
